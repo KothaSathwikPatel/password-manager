587e215887b54c690a12fea1a665089c621077b6
Kotha Sathwik Patel <sathwikpatelkotha01@gmail.com>
2026-08-13 02:06:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -107,3 +107,5 @@ generate_password_button.grid(row = 3 , column = 2)
 
 add_password_button = Button(text ="Add" , width =30 , command = save)
 add_password_button.grid(row = 4 , column = 1 , columnspan = 2)
+
+window.mainloop()
